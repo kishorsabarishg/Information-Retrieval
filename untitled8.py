f3c57deeb53410d4cf7d6806d92c28191d537674
kishorsabarishg <49763119+kishorsabarishg@users.noreply.github.com>
2019-10-13 16:30:04 +0530

    Add files via upload

---
+++ untitled8.py
@@ -0,0 +1,126 @@
+import numpy as np
+import operator
+import math
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.datasets import fetch_20newsgroups
+newsgroups=fetch_20newsgroups(subset='train',categories=
+                              ['alt.atheism','comp.graphics'])
+def reemovNestings(l): 
+    for i in l: 
+        if type(i) == list: 
+            reemovNestings(i) 
+        else: 
+            output.append(i)
+X = newsgroups.data
+query = input("Enter the Query term::\n")
+str(query)
+X.append(query)
+lemmatizer = WordNetLemmatizer()
+stop_words = list(stopwords.words("english"))
+new_X = []
+output = []
+N = len(X)
+punct_list = ["\n",",","(",")","-","[","]","*","{","}","#","@","!",":",";",".","?","|",">","<","+","*","&","'``'","--","_","^","%","/","\\","?","'",'"',"=","~"]
+for ngs in X:
+    ngs = ngs.lower()
+    for charac in punct_list:
+        ngs = ngs.replace(charac,' ') 
+    ngs = word_tokenize(ngs)
+    temp = []
+    for word in ngs:
+        if word not in stop_words:
+            temp.append(lemmatizer.lemmatize(word))
+    new_X.append(temp)
+X = new_X
+reemovNestings(new_X)
+temp_X = set()
+for k in output:
+    temp_X.add(k)
+vocab = dict.fromkeys(temp_X,1)
+count = dict.fromkeys(temp_X,0)
+i = 1
+for j in list(vocab.keys()):
+    vocab[j] = i
+    i+=1
+#for word in list(count.keys()):
+#    for doc in X:
+#        if word in doc:
+#            count[word]+=1
+for doc in X:    
+    for word in doc:
+        st = 1
+        k = doc.count(word)
+        if k > 1:
+            st = 0
+        if st == 1:
+            count[word] += 1
+        else:
+            count[word] += 1/k
+        math.ceil(count[word])
+T = len(vocab)
+T += 1
+#-----------------------------------IDF--------------------------------------#
+IDF = {}
+matrix_IDF = np.zeros((len(X),T))
+for words in list(vocab.keys()):
+    IDF[words] = math.log((N/count[words]),2)
+#matrix_IDF=np.ndarray((len(X),T))
+#-----------------------------------IDF--------------------------------------#
+
+
+
+#-----------------------------------TF-IDF-----------------------------------#
+doc_count = 0
+for doc in X:
+    n = len(doc)
+    for word in doc:
+        tf = 0
+        for word_2 in doc:
+            if word == word_2:
+                tf += 1
+        tf = tf/math.sqrt(tf)
+        tf = tf/n
+        matrix_IDF[doc_count][vocab[word]] = tf
+        matrix_IDF[doc_count][vocab[word]] *= IDF[word]
+    doc_count += 1
+#-----------------------------------TF-IDF-----------------------------------#
+    
+    
+#-----------------------------Query-Processing-------------------------------#
+matrix_q=np.zeros((T,))
+q_term = X[-1]
+q_length = len(q_term)
+for word in q_term:
+        tf_q=0
+        for word_2 in q_term:
+            if word == word_2:
+                tf_q+=1
+        tf_q = tf_q/math.sqrt(tf_q)
+        tf_q = tf/n
+        if tf_q > 0.001:
+            matrix_q[vocab[word]]=tf_q
+        else:
+            matrix_q[vocab[word]]=0
+#-----------------------------Query-Processing-------------------------------#
+
+
+#----------------------------Amplitude---------------------------------------#
+def amp(x):
+    s=0
+    for i in x:
+        if(i<0):
+           print(i)
+        s+=i**2
+    return math.sqrt(s)
+#----------------------------Amplitude---------------------------------------#
+    
+
+
+#------------------------Cosine-Similarity-----------------------------------#
+for index in range(N):
+    cos = float(np.dot(matrix_IDF[index],matrix_q))/(amp(matrix_IDF[index])*amp(matrix_q))
+    if cos > 0.0:
+        print(cos,index+1)
+#------------------------Cosine-Similarity-----------------------------------#
